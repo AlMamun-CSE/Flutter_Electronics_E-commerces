31c8440ade26493b879a0d6d6d212c176dac9dc6
AlMamun-CSE <almamuncse2020@gmail.com>
2021-09-30 14:48:20 +0600

    Complete Home Single Product

--- lib/screens/home/widgets/home_single_product.dart
+++ lib/screens/home/widgets/home_single_product.dart
@@ -61,6 +61,64 @@ class _HomeSingleProductState extends State<HomeSingleProduct> {
             SizedBox(
               height: 10,
             ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 25,
+                      width: 25,
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      height: 25,
+                      width: 25,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(5.0),
+                  width: 139,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "\$ 890.00",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "\$ 900",
+                        style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+
+            )
         ],
       ),
     );
